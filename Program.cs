6e468ae0f4b9ba5ae460ab4e0463952f3a4859f9
Pınar Kamit <pinarkamit@gmail.com>
2022-02-08 13:17:10 +0300

    degiskenler added,

--- Program.cs
+++ Program.cs
@@ -10,6 +10,15 @@
             Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
             Console.Write($"{Environment.NewLine}Press any key to exit...");
             Console.ReadKey(true);
+
+            byte havaDurumu = 26;
+            string isim = "Pınar";
+            double KDV = 0.18D;
+            float sicaklik = 25.48F;       
+            decimal uzaklık = 0.145M;
+            string uzaklik = "20";
+            int uzaklikInt = int.Parse(uzaklik);
+            string dateTime = DateTime.Now.ToString("dd.MM.yyyy");
         }
     }
 }
